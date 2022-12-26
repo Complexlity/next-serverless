8c53105eb27837ca460e57ad59762498facc6a2c
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-12-26 02:50:16 +0100

    Add second index code

--- pages/index2.js
+++ pages/index2.js
@@ -2,9 +2,10 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 export async function getStaticProps() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  let data = await res.json();
-  data = await data[0];
+  const res = await fetch(
+    "https://next-serverless-complexlity.vercel.app/api/hello"
+  );
+  const data = await res.json();
   return {
     props: {
       data,
